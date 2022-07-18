2bf15ffb7334fb391fbc4c851c2009a1f9f74b4c
Kashifshafati <shafatkashif@gmail.com>
2022-07-18 02:30:09 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import Review from './Review'
+import React,{useState} from 'react'
+import SingleQuestion from './Question'
+import List from './List'
 const App = () => {
-  
+  const [questions, setquestions] = useState(List)
+  console.log(questions)
   return (
     <main>
-    <section className='container'>
-      <div className='title'>
-        <h2>our reviews</h2>
-        <div className='underline'></div>
-      </div>
-      <Review />
-    </section>
+    <div className='container'>
+      <h3>questions and answers about login</h3>
+      <section className='info'>
+        {questions.map((question) => {
+          return (
+            <SingleQuestion key={question.id} {...question}></SingleQuestion>
+          );
+        })}
+      </section>
+    </div>
   </main>
   )
 }
