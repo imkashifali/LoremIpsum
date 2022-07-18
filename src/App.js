a415f59b153d4f45393f205056b718f9fa0aa70b
Kashifshafati <shafatkashif@gmail.com>
2022-07-18 00:07:11 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
-import React, { useState } from 'react'
-import Data from './Data'
-import List from './List'
-function App() {
-  const [pepole, setpepole] = useState(Data);
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+const url = "https://course-api.com/react-tours-project";
+
+const App = () => {
+  const [loading, setloading] = useState(true);
+  const [tours, settours] = useState([]);
+
+  const removeTour = (id) =>{
+   const newTour =  tours.filter((tour) => tour.id !== id)
+   settours(newTour)
+  }
+
+  const fetchTours = async () => {
+    setloading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      console.log(tours);
+      setloading(false);
+      settours(tours);
+    } catch (error) {
+      setloading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if(tours.length === 0){
+    return(
+      <main>
+      <div className='title'>
+        <h2>no tours left</h2>
+        <button className='btn' onClick={() => fetchTours()}>
+          refresh
+        </button>
+      </div>
+    </main>
+    )
+  }
   return (
     <main>
-      <section className='container'>
-        <h3> {pepole.length}birthdays today</h3>
-        <List  pepole={pepole}/>
-        <button onClick={()=> setpepole([])}>clear all</button>
-      </section>
+      <Tours tours={tours} removeTour={removeTour}/>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
