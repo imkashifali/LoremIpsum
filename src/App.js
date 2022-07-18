d0df2dd9e6a1dd0883c9242a05a8081bcd360afe
Kashifshafati <shafatkashif@gmail.com>
2022-07-18 05:35:26 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,70 +1,41 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
-function App() {
-  const [loading, setLoading] = useState(true)
-  const [jobs, setJobs] = useState([])
-  const [value, setValue] = useState(0)
+import React,{useState} from 'react'
+import Data from './Data'
+const App = () => {
+  const [count, setcount] = useState(0)
+    const [text, settext] = useState([])
 
-  const fetchJobs = async () => {
-    const reponse = await fetch(url)
-    const newJobs = await reponse.json()
-    setJobs(newJobs)
-    setLoading(false)
-  }
-  useEffect(() => {
-    fetchJobs()
-  }, [])
-  if (loading) {
-    return (
-      <section className="section loading">
-        <h1>Loading...</h1>
-      </section>
-    )
-  }
-  const { company, dates, duties, title } = jobs[value]
+
+    const handleSubmit= (e)=>{
+      e.preventDefault()
+      let amount = parseInt(count)
+      if(amount < 0){
+        amount =1
+      }
+      if(amount > 8){
+        amount = 8
+      }
+      settext(Data.slice(0,amount))
+    }
   return (
-    <section className="section">
-      <div className="title">
-        <h2>experience</h2>
-        <div className="underline"></div>
-      </div>
-      <div className="jobs-center">
-        {/* btn container */}
-        <div className="btn-container">
-          {jobs.map((item, index) => {
-            return (
-              <button
-                key={item.id}
-                onClick={() => setValue(index)}
-                className={`job-btn ${index === value && 'active-btn'}`}
-              >
-                {item.company}
-              </button>
-            )
-          })}
-        </div>
-        {/* job info */}
-        <article className="job-info">
-          <h3>{title}</h3>
-          <h4>{company}</h4>
-          <p className="job-date">{dates}</p>
-          {duties.map((duty, index) => {
-            return (
-              <div key={index} className="job-desc">
-                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
-                <p>{duty}</p>
-              </div>
-            )
-          })}
-        </article>
-      </div>
-      <button type="button" className="btn">
-        more info
-      </button>
-    </section>
+    <section className='section-center'>
+    <h3>tired of boring lorem ipsum?</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor='amount'>paragraphs:</label>
+      <input
+        type='number'
+        name='amount'
+        id='amount'
+        value={count}
+        onChange={(e) => setcount(e.target.value)}
+      />
+      <button className='btn'>generate</button>
+    </form>
+    <article className='lorem-text'>
+      {text.map((item, index) => {
+        return <p key={index}>{item}</p>;
+      })}
+    </article>
+  </section>
   )
 }
 
